7de5aab54544009cbcd294dc8f39b21274497ef2
k1195 <k1195>
2022-01-23 18:09:24 +0530

    asd

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const User = require('../model/usermodel')
-const Order = require('../model/userorder')
+const Order = require('../model/userOrder')
 const router = express.Router()
 const jwt = require('jsonwebtoken')
 const {JWT_SECRET} = require('../keys')
